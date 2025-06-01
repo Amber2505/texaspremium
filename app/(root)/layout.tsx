e6de2219401af3c5d3f9148b4fbfec59367411f0
Amber2505 <amberchatriwala2205@gmail.com>
2025-05-31 22:46:39 -0500

    fixed error and removed apostrophe  sign

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -296,9 +296,9 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         </div>
       </nav>
 
-      <div className="fixed bottom-4 right-4 bg-gray-800 text-white text-sm px-3 py-2 rounded-md shadow-lg z-50">
+      {/* <div className="fixed bottom-4 right-4 bg-gray-800 text-white text-sm px-3 py-2 rounded-md shadow-lg z-50">
         Screen: {screenSize.width}px × {screenSize.height}px
-      </div>
+      </div> */}
 
       {children}
       <footer className="bg-black text-white footer-text">
