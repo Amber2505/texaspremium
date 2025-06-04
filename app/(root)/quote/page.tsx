1857e9b0ee2ce33f2fd26ff99f7c5cb1d1b0a525
Amber2505 <amberchatriwala2205@gmail.com>
2025-06-04 08:23:51 -0500

    quote updated ?

--- app/(root)/quote/page.tsx
+++ app/(root)/quote/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Quote = () => {
   return (
     <div>
-      <h1 className='text-3xl'>What kind of quote are you trying to get</h1>
+      <h1 className="text-3xl">What kind of quote are you trying to get ?</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Quote
+export default Quote;
