4a0c383ffdf38d684d53ba2df1dba0848861abff
Amber2505 <amberchatriwala2205@gmail.com>
2025-06-15 00:17:44 -0500

    creating a shorten link putting the complete link in dest

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   async redirects(){
     return[{
       source: '/aZfGkRt',
-      destination:'?utm_source=flyers&utm_medium=distribution&utm_campaign=business_flyer&utm_id=business_flyer_dis&utm_term=business_fly_001&utm_content=business_fly_v1',
+      destination:'https://texaspremiumins.com?utm_source=flyers&utm_medium=distribution&utm_campaign=business_flyer&utm_id=business_flyer_dis&utm_term=business_fly_001&utm_content=business_fly_v1',
       permanent: true,
     }]
 
