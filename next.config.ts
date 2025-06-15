871a49f14f0f6a6a1b0b43dbde0b46c56ee70092
Amber2505 <amberchatriwala2205@gmail.com>
2025-06-15 00:12:51 -0500

    creating a shorten link

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects(){
+    return[{
+      source: '/aZfGkRt',
+      destination:'?utm_source=flyers&utm_medium=distribution&utm_campaign=business_flyer&utm_id=business_flyer_dis&utm_term=business_fly_001&utm_content=business_fly_v1',
+      permanent: true,
+    }]
+
+  }
 };
 
 export default nextConfig;
